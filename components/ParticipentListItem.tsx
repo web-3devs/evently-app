7e9935eaf399fdc154b1c37afc5cc8d8447a0436
VIVEK-SUTHAR <sutharvivek18@gmail.com>
2022-11-15 11:00:36 +0530

    app 95% done

--- components/ParticipentListItem.tsx
+++ components/ParticipentListItem.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, TouchableWithoutFeedback, View } from "react-native";
 import { Avatar, Button } from "react-native-rapi-ui";
 import { AntDesign } from "@expo/vector-icons";
 
@@ -22,43 +22,48 @@ const ParticipentListItem = ({
   setpid,
   setmail
 }: ParticipentListItemProps): React.ReactElement => {
+  const [seed, setSeed] = useState<number>(0);
+  useEffect(() => {
+    return setSeed(Math.floor(Math.random() * 5000));
+  }, []);
   return (
-    <View
-      style={{
-        backgroundColor: "white",
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-between",
-        paddingHorizontal: 10,
-        marginVertical: 5,
-        paddingVertical: 10,
-        borderRadius: 4,
-        borderColor: "black",
-        borderWidth: 1,
-      }}
-      onTouchEnd={(e) => {
-        showSheet((prev: any) => !prev);
-        setP_name(name);
-        setmail(email)
-        setpid(p_id);
-      }}
-    >
-      <View style={{ display: "flex", flexDirection: "row" }}>
-        <Avatar
+    <TouchableWithoutFeedback onPress={() => {
+      showSheet((prev: any) => !prev);
+      setP_name(name);
+      setmail(email);
+      setpid(p_id);
+    }}>
+      <View
+        style={{
+          backgroundColor: "white",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+          paddingHorizontal: 10,
+          marginVertical: 5,
+          paddingVertical: 10,
+          borderRadius: 4,
+          borderColor: "black",
+          borderWidth: 1,
+        }}
+       
+      >
+        <View style={{ display: "flex", flexDirection: "row" }}>
+          {/* <Avatar
           source={{
-            uri: "https://viveksuthar.me/static/media/Pic.5cc5bb4227ecb7c226ae.webp",
+            uri: `https://avatars.dicebear.com/api/human/${seed}.svg`,
           }}
           size="md"
-        />
-        <View style={{ marginHorizontal: 8 }}>
-          <Text style={{ fontSize: 16, fontWeight: "700" }}>{name}</Text>
-          <Text style={{ fontSize: 12, fontWeight: "400", color: "gray" }}>
-            {email}
-          </Text>
+        /> */}
+          <View style={{ marginHorizontal: 8 }}>
+            <Text style={{ fontSize: 17, fontWeight: "700" }}>{name}</Text>
+            <Text style={{ fontSize: 14, fontWeight: "400", color: "gray" }}>
+              {email}
+            </Text>
+          </View>
         </View>
       </View>
-      <AntDesign name="checkcircle" size={36} color="#38E54D" />
-    </View>
+    </TouchableWithoutFeedback>
   );
 };
 
